a1755fadfa11e6757421bfcbc97172cd26d61694
webbtrain <adam-github@webbtrain.us>
2024-12-25 11:40:28 -0600

    mods

--- 20241225.go
+++ 20241225.go
@@ -5,8 +5,6 @@ import (
 )
 
 func has_pair_with_sum(data []int, sum int) bool {
-	// O(n) time complexity
-	// O(1) space complexity
 	for i := 0; i < len(data); i++ {
 		for j := i + 1; j < len(data); j++ {
 			if data[i]+data[j] == sum {
